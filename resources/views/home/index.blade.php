f9df8fdebbe72b2527ed3a6eb32094a23c3a55fb
Noelierx <noelieroux11@gmail.com>
2018-03-11 21:40:35 +0100

    new alravel

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,27 +10,15 @@
 
 @section('page-title', 'Profil')
 
-<!-- CONNEXION -->
-	<!-- HERO SECTION  -->
-	<div class="site-hero_2 parallax bg-dark">
-		<div class="page-title">
-			<div class="big-title white-text">Title</div>
-			<div class="small-title white-text">home / blog / single_post</div>
-		</div>
-	</div>
-
 	<!-- POSTS -->
 	<section>
 		<div class="container mt-100 mb-50">
 			<div class="row">
 				<div class="col-md-9 col-sm-12">
 					<div class="single_post">
-						<div class="post_media">
-							<img src="assets/img/blog_post.jpg" alt="post image">
-						</div>
 						<div class="post_title">
-							<h4 class="montserrat-text uppercase">Duis aute irure dolor in reprehenderit.</h4>
-							<span class="post_date">Date : 2017-02-3</span>
+							<h4 class="montserrat-text uppercase">Explications du site</h4>
+							<span class="post_date">Pour vous guider.</span>
 						</div>
 						<p>
 							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -46,6 +34,7 @@
 						<p>
 							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia sint placeat praesentium dolorum minima, et laborum similique cupiditate minus nemo qui tempore corporis eum quisquam harum perferendis saepe tempora ipsum. Quia sint placeat praesentium dolorum minima, et laborum similique cupiditate minus nemo qui tempore corporis eum quisquam harum perferendis saepe tempora ipsum. Quia sint placeat praesentium dolorum minima, et laborum similique cupiditate minus nemo qui tempore corporis eum quisquam harum perferendis saepe tempora ipsum. Quia sint placeat praesentium dolorum minima, et laborum similique cupiditate minus nemo qui tempore corporis eum quisquam harum perferendis saepe tempora ipsum.
 						</p>
+                        <br/><br/><br/>
 					</div>
                 </div>
             </div>
