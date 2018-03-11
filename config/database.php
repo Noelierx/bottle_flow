69216bff4c8e66e60324adf97fbc8fc13f0121cd
Noelierx <noelieroux11@gmail.com>
2018-03-11 16:12:06 +0100

    new

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'eemi.tech'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'nroux'),
             'username' => env('DB_USERNAME', 'nroux'),
