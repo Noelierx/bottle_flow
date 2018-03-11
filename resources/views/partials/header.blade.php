5250418621e84ded635f0f301070dc408eff055a
Siliace <benjber69@gmail.com>
2018-03-11 23:11:25 +0100

    Add good link to home in the header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
 	<header class="main-header">
 		<div class="container">
 			<div class="logo">
-				<a href="index.html"><img src="{{ asset('img/logo.png') }}" width="150" height="auto" alt="logo"></a>
+				<a href="{{ route('index') }}"><img src="{{ asset('img/logo.png') }}" width="150" height="auto" alt="logo"></a>
 			</div>
             @include('partials.nav')
 		</div>
