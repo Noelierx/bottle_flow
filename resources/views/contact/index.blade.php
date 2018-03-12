a802f5a9b88c5240551bcdb47ccf60dc057126f3
Noëlie <noelieroux11@gmail.com>
2018-03-12 16:22:40 +0100

    Add ressources integration

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,24 +1,44 @@
 @extends('default')
+<!-- HERO SECTION  -->
+	<div class="site-hero_2 parallax bg-dark">
+		<div class="page-title">
+			<div class="big-title white-text">Panier</div>
+			<div class="small-title white-text">Lorem Ipsum</div>
+		</div>
+	</div>
 
-@section('content')
-    @include('partials.error')
-    {!! html()->form()->action(route('contact.store'))->open() !!}
-    <div>
-        {!! html()->label('Votre prénom', 'first_name') !!}
-        {!! html()->text('first_name')->required() !!}
-    </div>
-    <div>
-        {!! html()->label('Votre nom', 'last_name') !!}
-        {!! html()->text('last_name')->required() !!}
-    </div>
-    <div>
-        {!! html()->label('Votre email', 'email') !!}
-        {!! html()->email('email')->required() !!}
-    </div>
-    <div>
-        {!! html()->label('Votre message', 'content') !!}
-        {!! html()->textarea('content')->required() !!}
-    </div>
-    {!! html()->submit('Envoyer') !!}
-    {!! html()->form()->close() !!}
-@endsection
+@include('partials.error')
+@section('page-title', 'Panier')
+
+	<!-- POSTS -->
+	<section>
+		<div class="container mt-100 mb-50">
+			<div class="row">
+				<div class="col-md-12 col-sm-12">
+					<div class="single_post">
+						 {!! html()->form()->action(route('contact.store'))->open() !!}
+                        <div>
+                            {!! html()->label('Votre prénom', 'first_name') !!}
+                            {!! html()->text('first_name')->required() !!}
+                        </div>
+                        <div>
+                            {!! html()->label('Votre nom', 'last_name') !!}
+                            {!! html()->text('last_name')->required() !!}
+                        </div>
+                        <div>
+                            {!! html()->label('Votre email', 'email') !!}
+                            {!! html()->email('email')->required() !!}
+                        </div>
+                        <div>
+                            {!! html()->label('Votre message', 'content') !!}
+                            {!! html()->textarea('content')->required() !!}
+                        </div>
+                        {!! html()->submit('Envoyer') !!}
+                        {!! html()->form()->close() !!}
+                        <br/><br/><br/><br/><br/><br/>
+					</div>
+                </div>
+            </div>
+        </div>
+</section>
+    
