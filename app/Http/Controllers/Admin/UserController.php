96e3e93236b234f909e76411b4875f4b32bbc57c
Siliace <benjber69@gmail.com>
2018-03-12 23:12:45 +0100

    Implement user update

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -37,6 +37,37 @@ class UserController extends Controller
         return redirect()->route('admin.users.index');
     }
 
+    public function edit(User $user)
+    {
+        return view('admin.user.edit', [
+            'user' => $user
+        ]);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $this->validate($request, [
+            'pseudo' => "required|string|min:6|max:255|unique:users,pseudo,$user->id,id",
+            'email' => "required|email|min:6|max:255|unique:users,email,$user->id,id",
+            'password' => 'nullable|min:6|max:255|confirmed',
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'country' => 'required|string|max:255',
+        ]);
+
+        $user->fill($request->except('password'));
+
+        if (!is_null($request->get('password'))) {
+            $user->password = bcrypt($request->get('password'));
+        }
+
+        $user->is_admin = $request->get('is_admin', false);
+
+        $user->save();
+
+        return redirect()->route('admin.users.index');
+    }
+
     public function destroy(User $user)
     {
         try {
---
+++ resources/views/admin/user/edit.blade.php
@@ -0,0 +1,42 @@
+@extends('admin.default')
+
+@section('content')
+    @include('admin.partials.error')
+    {!! html()->form('PUT', route('admin.users.update', $user))->open() !!}
+    <div>
+        {!! html()->label('Pseudo', 'pseudo') !!}
+        {!! html()->text('pseudo', $user->pseudo)->required() !!}
+    </div>
+    <div>
+        {!! html()->label('Email', 'email') !!}
+        {!! html()->email('email', $user->email)->required() !!}
+    </div>
+    <div>
+        {!! html()->label('Prénom', 'first_name') !!}
+        {!! html()->text('first_name', $user->first_name)->required() !!}
+    </div>
+    <div>
+        {!! html()->label('Nom de famille', 'last_name') !!}
+        {!! html()->text('last_name', $user->last_name)->required() !!}
+    </div>
+    <div>
+        {!! html()->label('Mot de passe', 'password') !!}
+        {!! html()->password('password') !!}
+    </div>
+    <div>
+        {!! html()->label('Confirmation du mot de passe', 'password_confirmation') !!}
+        {!! html()->password('password_confirmation') !!}
+    </div>
+    <div>
+        {!! html()->label('Pays', 'country') !!}
+        {!! html()->select('country', ['France' => 'France', 'Belgique' => 'Belgique'], $user->country)->required() !!}
+    </div>
+    <div>
+        {!! html()->label('Status')->addChild(html()->checkbox('status', $user->status)) !!}
+    </div>
+    <div>
+        {!! html()->label('Administrateur')->addChild(html()->checkbox('is_admin', $user->is_admin)) !!}
+    </div>
+    {!! html()->submit('Modifier') !!}
+    {!! html()->form()->close() !!}
+@endsection
