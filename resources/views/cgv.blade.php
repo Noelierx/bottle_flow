58a8ef7afd4ab4d6d7ce7cbd0cd334a3c6dd68db
Siliace <benjber69@gmail.com>
2018-03-18 18:25:10 +0100

    Fix CGV

--- resources/views/cgv.blade.php
+++ resources/views/cgv.blade.php
@@ -1,86 +1,104 @@
 @extends('default')
-<!-- HERO SECTION  -->
-	<div class="site-hero_2 parallax bg-dark">
-		<div class="page-title">
-			<div class="big-title white-text">Conditions générales de ventes</div>
-			<div class="small-title white-text">Lorem Ipsum</div>
-		</div>
-	</div>
 
+@section('page-title', 'CGV'))
+@section('hero-title', 'Conditions générales de ventes')
+@section('hero-subtitle', 'Lorem Ipsum')
 
-@section('page-title', 'CGV')
-
-	<!-- POSTS -->
-	<section>
-		<div class="container mt-100 mb-50">
-			<div class="row text-center">
-				<div class="col-md-12">
-						<p>
-                            <strong>ARTICLE 1 : Mentions obligatoires</strong><br/>
-                            Le site Internet bottleflowproject.herokuapp.com est un service de :
-                        </p>
-                            <ul>
-                                <li>e-commerce</li>
-                                <li>28 place de la Bourse, 75002 Paris, France</li>
-                                <li>Immatriculée au RCS de Nanterre : 823 575 030 00015</li>
-                                <li>Numéro d'identification fiscale : 7022Z</li>
-                                <li>01 83 92 20 20</li>
-                            </ul>
-                        <p>
-                            <strong>ARTICLE 2 : Caractéristiques essentielles des produits et services vendus</strong><br/>
-                            Le site Internet bottleflowproject.herokuapp.com vend :
-                            Des améliorations concernant les contenants des messages envoyés
-                            Des améliorations concernant les contenus des messages envoyés
-                            Le client déclare avoir pris connaissance et avoir accepté les conditions générales de vente antérieurement à la passation de sa commande. La validation de votre commande vaut donc pour acceptation des conditions générales de vente.
-                        </p>
-						<p>
-                            <strong>ARTICLE 3 : Prix</strong><br/>
-                            Les prix de nos produits sont indiqués en euros toutes taxes comprises (TTC).
-                            En cas de commande livrée dans un pays autre que la France métropolitaine, le client est l'importateur des produits qu’il achète. Pour tous les produits expédiés hors des collectivités d’outre-mer ou de l’Union européenne, la facture est établie sur le prix hors taxes. Le client est seul responsable des déclarations et paiements de tout droit de douane ou autre taxe susceptibles d'être exigibles dans son pays.
-                            Selon le cas :
-                            Les frais de livraison ne sont pas compris dans le prix. Le bon de commande indique le montant des frais de livraison avant toute 	passation de commande.
-                            Les frais de livraison sont inclus dans les prix des produits proposés en ligne.
-                        </p>
-                        <p>
-                            <strong>ARTICLE 4 : Délai de disponibilité des produits</strong><br/>
-                            Les produits disponibles apparaissent sur notre site accompagnés de la mention « disponible ». Afin de répondre au mieux aux attentes de notre clientèle, la disponibilité de nos produits est régulièrement mise à jour sur notre site internet.
-                            Si vous avez commandé un produit indisponible postérieurement à la validation de votre commande, vous en serez immédiatement informé. Nous procéderons à l'annulation de votre achat.  
-                            Si la somme avait déjà été débitée, vous serez immédiatement remboursé.
-                        </p>
-                        <p>
-                            <strong>ARTICLE 5 : Commande</strong><br/>
-                            Pour passer une commande sur notre site, choisissez vos articles et ajoutez-les au panier. Validez le contenu de votre panier.
-                            Selon le cas :
-                            Si vous possédez déjà un compte client sur notre site, veuillez vous identifier.
-                            Si vous ne possédez pas de compte client sur notre site, veuillez 	en créer un.
-                            Selon le cas :
-                            Cochez la case « paiement » pour honorer votre commande en ligne.
-                            Choisissez votre mode de paiement et cochez la case « acceptation des CGV ».
-                            Validez votre paiement.
-                            Vous recevrez un e-mail de confirmation de votre commande sur l'adresse mail que vous avez indiquée lors de la création de votre compte client.
-                            Vérifiez les détails et le montant total de votre commande. Rectifiez au préalable les éventuelles erreurs avant toute acceptation.
-                            Le transfert de propriété du produit n'aura lieu qu'au paiement complet de votre commande.
-                        </p>
-                        <p>
-                            <strong>ARTICLE 6 : Livraison</strong><br/>
-                            Selon le cas :
-                            Nous effectuons nos livraisons directement via le site internet puisqu'il s'agit d'accessoires de personnalisation de vos messages.
-                            Les délais de livraison sont donnés à titre indicatif. Ils peuvent évoluer en raison de différents facteurs comme [la disponibilité des produits / du traitement de la commande, etc.].
-                        </p>
-                        <p>
-                            <strong>ARTICLE 7 : Modalités de paiement</strong><br/>
-                            Selon le cas :
-                            Plusieurs moyens de paiement sont acceptés. En tant que client, vous avez la possibilité de payer par : carte bancaire / PayPal/ Stripe.
-                            Selon le cas :
-                            Nous acceptons les paiements par carte bleue / visa / master card / eurocard. Lors de votre paiement en ligne, indiquez le numéro de votre carte bancaire, sa date 	d'expiration et les 3 chiffres inscrits au dos de celle-ci. Le paiement en ligne est sécurisé.
-                            Éventuellement :
-                            Nous exigeons un paiement intégral de la commande.
-                        </p>
-						<blockquote class="bq playfair-text">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia sint placeat praesentium dolorum minima, et laborum similique cupiditate minus nemo qui tempore corporis eum quisquam harum perferendis saepe tempora ipsum, et laborum similique cupiditate minus nemo qui tempore corporis eum quisquam harum perferendis saepe tempora ipsum. Quia sint placeat praesentium dolorum minima, et laborum similique cupiditate minus nemo qui tempore corporis eum quisquam harum perferendis saepe tempora ipsum.
-						</blockquote>
-                        <br/><br/><br/>
-					</div>
+@section('content')
+    <section>
+        <div class="container mt-100 mb-50">
+            <div class="row text-center">
+                <div class="col-md-12">
+                    <p>
+                        <strong>ARTICLE 1 : Mentions obligatoires</strong><br/>
+                        Le site Internet bottleflowproject.herokuapp.com est un service de :
+                    </p>
+                    <ul>
+                        <li>e-commerce</li>
+                        <li>28 place de la Bourse, 75002 Paris, France</li>
+                        <li>Immatriculée au RCS de Nanterre : 823 575 030 00015</li>
+                        <li>Numéro d'identification fiscale : 7022Z</li>
+                        <li>01 83 92 20 20</li>
+                    </ul>
+                    <p>
+                        <strong>ARTICLE 2 : Caractéristiques essentielles des produits et services vendus</strong><br/>
+                        Le site Internet bottleflowproject.herokuapp.com vend :
+                        Des améliorations concernant les contenants des messages envoyés
+                        Des améliorations concernant les contenus des messages envoyés
+                        Le client déclare avoir pris connaissance et avoir accepté les conditions générales de vente
+                        antérieurement à la passation de sa commande. La validation de votre commande vaut donc pour
+                        acceptation des conditions générales de vente.
+                    </p>
+                    <p>
+                        <strong>ARTICLE 3 : Prix</strong><br/>
+                        Les prix de nos produits sont indiqués en euros toutes taxes comprises (TTC).
+                        En cas de commande livrée dans un pays autre que la France métropolitaine, le client est
+                        l'importateur des produits qu’il achète. Pour tous les produits expédiés hors des collectivités
+                        d’outre-mer ou de l’Union européenne, la facture est établie sur le prix hors taxes. Le client
+                        est seul responsable des déclarations et paiements de tout droit de douane ou autre taxe
+                        susceptibles d'être exigibles dans son pays.
+                        Selon le cas :
+                        Les frais de livraison ne sont pas compris dans le prix. Le bon de commande indique le montant
+                        des frais de livraison avant toute passation de commande.
+                        Les frais de livraison sont inclus dans les prix des produits proposés en ligne.
+                    </p>
+                    <p>
+                        <strong>ARTICLE 4 : Délai de disponibilité des produits</strong><br/>
+                        Les produits disponibles apparaissent sur notre site accompagnés de la mention « disponible ».
+                        Afin de répondre au mieux aux attentes de notre clientèle, la disponibilité de nos produits est
+                        régulièrement mise à jour sur notre site internet.
+                        Si vous avez commandé un produit indisponible postérieurement à la validation de votre commande,
+                        vous en serez immédiatement informé. Nous procéderons à l'annulation de votre achat.
+                        Si la somme avait déjà été débitée, vous serez immédiatement remboursé.
+                    </p>
+                    <p>
+                        <strong>ARTICLE 5 : Commande</strong><br/>
+                        Pour passer une commande sur notre site, choisissez vos articles et ajoutez-les au panier.
+                        Validez le contenu de votre panier.
+                        Selon le cas :
+                        Si vous possédez déjà un compte client sur notre site, veuillez vous identifier.
+                        Si vous ne possédez pas de compte client sur notre site, veuillez en créer un.
+                        Selon le cas :
+                        Cochez la case « paiement » pour honorer votre commande en ligne.
+                        Choisissez votre mode de paiement et cochez la case « acceptation des CGV ».
+                        Validez votre paiement.
+                        Vous recevrez un e-mail de confirmation de votre commande sur l'adresse mail que vous avez
+                        indiquée lors de la création de votre compte client.
+                        Vérifiez les détails et le montant total de votre commande. Rectifiez au préalable les
+                        éventuelles erreurs avant toute acceptation.
+                        Le transfert de propriété du produit n'aura lieu qu'au paiement complet de votre commande.
+                    </p>
+                    <p>
+                        <strong>ARTICLE 6 : Livraison</strong><br/>
+                        Selon le cas :
+                        Nous effectuons nos livraisons directement via le site internet puisqu'il s'agit d'accessoires
+                        de personnalisation de vos messages.
+                        Les délais de livraison sont donnés à titre indicatif. Ils peuvent évoluer en raison de
+                        différents facteurs comme [la disponibilité des produits / du traitement de la commande, etc.].
+                    </p>
+                    <p>
+                        <strong>ARTICLE 7 : Modalités de paiement</strong><br/>
+                        Selon le cas :
+                        Plusieurs moyens de paiement sont acceptés. En tant que client, vous avez la possibilité de
+                        payer par : carte bancaire / PayPal/ Stripe.
+                        Selon le cas :
+                        Nous acceptons les paiements par carte bleue / visa / master card / eurocard. Lors de votre
+                        paiement en ligne, indiquez le numéro de votre carte bancaire, sa date d'expiration et les 3
+                        chiffres inscrits au dos de celle-ci. Le paiement en ligne est sécurisé.
+                        Éventuellement :
+                        Nous exigeons un paiement intégral de la commande.
+                    </p>
+                    <blockquote class="bq playfair-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia sint placeat praesentium dolorum
+                        minima, et laborum similique cupiditate minus nemo qui tempore corporis eum quisquam harum
+                        perferendis saepe tempora ipsum, et laborum similique cupiditate minus nemo qui tempore corporis
+                        eum quisquam harum perferendis saepe tempora ipsum. Quia sint placeat praesentium dolorum
+                        minima, et laborum similique cupiditate minus nemo qui tempore corporis eum quisquam harum
+                        perferendis saepe tempora ipsum.
+                    </blockquote>
+                    <br/><br/><br/>
                 </div>
             </div>
-</section>
+        </div>
+    </section>
+@endsection
