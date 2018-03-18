594e3a08e41d145015d634826692d17e4580847a
Siliace <benjber69@gmail.com>
2018-03-18 16:59:44 +0100

    Fix config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Bottle flow'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://bottle-flow-project.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
