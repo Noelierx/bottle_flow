f0fef5b1d6d5b1ec6edfdcb8b7058dc4a49abf0c
Siliace <benjber69@gmail.com>
2018-03-18 17:57:32 +0100

    Fix missing middlewares

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -6,6 +6,11 @@ use Auth;
 
 class InventoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('inventory.index', [
